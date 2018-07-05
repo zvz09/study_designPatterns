c3f76247c4d776f597d02a1c3f3d81a00780c425
zvz <18237119598@163.com>
2018-07-06 07:18:30 +0800

    模板方法模式

---
+++ TemplateMethodPattern/src/main/java/com/zvz/study/tmp/Client.java
@@ -0,0 +1,14 @@
+package com.zvz.study.tmp;
+
+import com.zvz.study.tmp.model.HummerH1Model;
+import com.zvz.study.tmp.model.HummerModel;
+
+public class Client {
+
+    public static void main(String[] args) {
+//XX公司要H1型号的悍马
+        HummerModel h1 = new HummerH1Model();
+//H1模型演示
+        h1.run();
+    }
+}
