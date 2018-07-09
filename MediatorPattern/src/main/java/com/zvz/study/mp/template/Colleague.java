8db06368d816112c9f5688ff9f073b31b91b3016
zvz <18237119598@163.com>
2018-07-10 07:30:54 +0800

    原型模式  中介者模式

---
+++ MediatorPattern/src/main/java/com/zvz/study/mp/template/Colleague.java
@@ -0,0 +1,8 @@
+package com.zvz.study.mp.template;
+
+public abstract class Colleague {
+    protected Mediator mediator;
+    public Colleague(Mediator _mediator){
+        this.mediator = _mediator;
+    }
+}
---
+++ MediatorPattern/src/main/java/com/zvz/study/mp/template/ConcreteMediator.java
@@ -0,0 +1,15 @@
+package com.zvz.study.mp.template;
+
+public class ConcreteMediator extends Mediator {
+    @Override
+    public void doSomething1() {
+//调用同事类的方法，只要是public方法都可以调用
+        super.c1.selfMethod1();
+        super.c2.selfMethod2();
+    }
+    @Override
+    public void doSomething2() {
+        super.c1.selfMethod1();
+        super.c2.selfMethod2();
+    }
+}
