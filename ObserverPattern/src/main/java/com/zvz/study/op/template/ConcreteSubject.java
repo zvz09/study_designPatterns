09823014b966e207210205c1abb058567e9607f9
zvz <18237119598@163.com>
2018-07-15 15:05:04 +0800

    观察者模式（Observer Pattern）

---
+++ ObserverPattern/src/main/java/com/zvz/study/op/template/ConcreteSubject.java
@@ -0,0 +1,11 @@
+package com.zvz.study.op.template;
+
+public class ConcreteSubject extends Subject {
+    //具体的业务
+    public void doSomething(){
+        /*
+         * do something
+         */
+        super.notifyObservers();
+    }
+}
---
+++ ObserverPattern/src/main/java/com/zvz/study/op/template/Observer.java
@@ -0,0 +1,6 @@
+package com.zvz.study.op.template;
+
+public interface Observer {
+    //更新方法
+    public void update();
+}
