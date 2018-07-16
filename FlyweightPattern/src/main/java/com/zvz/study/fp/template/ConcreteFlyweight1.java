ebf825cb6cbc1a75e0d9d2a29cb770810f049e51
zvz <18237119598@163.com>
2018-07-17 07:52:52 +0800

    享元模式

---
+++ FlyweightPattern/src/main/java/com/zvz/study/fp/template/ConcreteFlyweight1.java
@@ -0,0 +1,13 @@
+package com.zvz.study.fp.template;
+
+public class ConcreteFlyweight1 extends Flyweight{
+    //接受外部状态
+    public ConcreteFlyweight1(String _Extrinsic){
+        super(_Extrinsic);
+    }
+    //根据外部状态进行逻辑处理
+    @Override
+    public void operate(){
+//业务逻辑
+    }
+}
